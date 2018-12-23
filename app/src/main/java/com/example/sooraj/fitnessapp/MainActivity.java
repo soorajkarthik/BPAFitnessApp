4e3ebb0911729efd98d1b20492631c87bf1e8dad
Sooraj Karthik <soorajkarthik15@gmail.com>
2018-12-23 16:01:52 -0600

    Change theme
    Makes ui look more polished

--- app/src/main/java/com/example/sooraj/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/sooraj/fitnessapp/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.sooraj.fitnessapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.sooraj.fitnessapp.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/sooraj/fitnessapp/Model/User.java
+++ app/src/main/java/com/example/sooraj/fitnessapp/Model/User.java
@@ -6,6 +6,22 @@ public class User {
     private String username;
     private String password;
 
+    private int weight;
+    private int height;
+    private int age;
+    private int bmi;
+    private int steps;
+    private int stepGoal;
+    private int caloriesBurned;
+    private int calories;
+    private int calorieGoal;
+    private int carbs;
+    private int carbGoal;
+    private int protein;
+    private int proteinGoal;
+    private int fat;
+    private int fatGoal;
+
     public User() {
 
     }
@@ -17,18 +33,22 @@ public class User {
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getUsername() {
+
         return username;
     }
 
     public void setUsername(String username) {
+
         this.username = username;
     }
 
@@ -37,9 +57,127 @@ public class User {
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
+    public int getWeight() {
+        return weight;
+    }
 
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(int bmi) {
+        this.bmi = bmi;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public int getStepGoal() {
+        return stepGoal;
+    }
+
+    public void setStepGoal(int stepGoal) {
+        this.stepGoal = stepGoal;
+    }
+
+    public int getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(int caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getCalorieGoal() {
+        return calorieGoal;
+    }
+
+    public void setCalorieGoal(int calorieGoal) {
+        this.calorieGoal = calorieGoal;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(int carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getCarbGoal() {
+        return carbGoal;
+    }
+
+    public void setCarbGoal(int carbGoal) {
+        this.carbGoal = carbGoal;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
+
+    public int getProteinGoal() {
+        return proteinGoal;
+    }
+
+    public void setProteinGoal(int proteinGoal) {
+        this.proteinGoal = proteinGoal;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public int getFatGoal() {
+        return fatGoal;
+    }
+
+    public void setFatGoal(int fatGoal) {
+        this.fatGoal = fatGoal;
+    }
 }
